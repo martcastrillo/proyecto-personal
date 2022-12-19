3416ab15f1a2a477a603e80a8892ba55061eb29f
Marta Castrillo <mart.castrillo@gmail.com>
2022-12-19 23:30:50 +0100

    Publish in GitHub Pages

--- src/components/Horoscopes/Leo.js
+++ src/components/Horoscopes/Leo.js
@@ -1,48 +1,53 @@
 import "../../styles/GenericHorocscope.scss";
 import { Link } from "react-router-dom";
-import leomarco from "../../images/marco/leomarco.png"
+import leomarco from "../../images/marco/leomarco.png";
 import handleft from "../../images/handleft.png";
 import handright from "../../images/handright.png";
 const Leo = () => {
 	return (
 		<div className="background">
-		<header className="header">
-		<Link to="/" className="link header">
-				
-				
-		
-				<img className="logo" src={handleft} alt="logo" title="rick-and" />
-				<h1 className="h1">horóscopo</h1>
-				<img className="logo" src={handright} alt="logo" title="rick-and" />
+			<header className="header">
+				<Link to="/" className="link header">
+					<img className="logo" src={handleft} alt="logo" title="rick-and" />
+					<h1 className="h1">horóscopo</h1>
+					<img className="logo" src={handright} alt="logo" title="rick-and" />
 				</Link>
 			</header>
-		<div className="generichoroscope">
-			<div className="generichoroscope__divivage">
-				<img src={leomarco}alt="" className="generichoroscope__divimage__image"/>
-			</div>
-			<div>
-			<div className="generichoroscope__text">
-			<h2 className="generichoroscope__h2">Características</h2>
-				<p className='generichoroscope-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui maiores
-				consequatur dolor aperiam fugiat voluptas, soluta, repellendus assumenda
-				asperiores exercitationem minus labore ducimus unde quo veniam
-				doloremque itaque tempora eum?</p>
-				<p className='generichoroscope-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui maiores
-				consequatur dolor aperiam fugiat voluptas, soluta, repellendus assumenda
-				asperiores exercitationem minus labore ducimus unde quo veniam
-				doloremque itaque tempora eum?</p>
-				<p className='generichoroscope-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui maiores
-				consequatur dolor aperiam fugiat voluptas, soluta, repellendus assumenda
-				asperiores exercitationem minus labore ducimus unde quo veniam
-				doloremque itaque tempora eum?</p>
-				
-			</div>
-			<Link to="/" className="link">
-				
-					<i className="fa-solid fa-chevron-left"></i>Return
-				</Link>
+			<div className="generichoroscope">
+				<div className="generichoroscope__divivage">
+					<img
+						src={leomarco}
+						alt=""
+						className="generichoroscope__divimage__image"
+					/>
 				</div>
-		</div>
+				<div>
+					<div className="generichoroscope__text">
+						<h2 className="generichoroscope__h2">Características</h2>
+						<p className="generichoroscope-paragraph">
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui
+							maiores consequatur dolor aperiam fugiat voluptas, soluta,
+							repellendus assumenda asperiores exercitationem minus labore
+							ducimus unde quo veniam doloremque itaque tempora eum?
+						</p>
+						<p className="generichoroscope-paragraph">
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui
+							maiores consequatur dolor aperiam fugiat voluptas, soluta,
+							repellendus assumenda asperiores exercitationem minus labore
+							ducimus unde quo veniam doloremque itaque tempora eum?
+						</p>
+						<p className="generichoroscope-paragraph">
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui
+							maiores consequatur dolor aperiam fugiat voluptas, soluta,
+							repellendus assumenda asperiores exercitationem minus labore
+							ducimus unde quo veniam doloremque itaque tempora eum?
+						</p>
+					</div>
+					<Link to="/" className="link">
+						<i className="fa-solid fa-chevron-left"></i>Return
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
