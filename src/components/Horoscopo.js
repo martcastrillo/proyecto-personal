507a5e63982532464c7deed5e8e4680353053d3c
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-10 11:25:50 +0200

    add first steps to filter

--- src/components/Horoscopo.js
+++ src/components/Horoscopo.js
@@ -1,7 +1,34 @@
 import { Link } from "react-router-dom";
+import {  useState } from 'react';
 import datadev from "../services/data.json";
 const Inicio = () => {
-	const htmlData = datadev.map((horoscope, index) => {
+	const [group, setGroup] = useState('todos');
+/* 	const handleSearchGroup = (ev) => {
+		setGroup(ev.target.value);
+		console.log(ev.target.value);
+	  } */
+	  const handleGroup = (ev) => {
+		setGroup(ev.target.id);
+		console.log(ev.target.id);
+	  }
+	const htmlData = datadev
+    .filter((filterGroup) => {
+      let result = '';
+	  
+      if (group === 'todos') {
+		console.log('todos');
+		result = true;
+        return result;
+      }
+      else if (filterGroup.type === group) {
+        console.log(filterGroup.type );
+		result = true;
+        return result;
+		
+      }
+      return result;
+    })
+	.map((horoscope, index) => {
 		
 		return (
 			<Link to={horoscope.link} className="onehoroscope" key={index}>
@@ -13,16 +40,23 @@ const Inicio = () => {
 
 	return (
 		<div className="background-main">
-			<form action="" className="form">
-				<input type="radio" />
+			<div onClick={handleGroup} id="todos">Todos</div>
+			<div onClick={handleGroup} id="fire">Fuego</div>
+			<div onClick={handleGroup} id="earth">Tierra</div>
+			<div onClick={handleGroup} id="agua">Agua</div>
+			<div onClick={handleGroup} id="air">Aire</div>
+			{/* <form action="" className="form">
+			<input type="checkbox"value="todos" id="todos" onChange={handleSearchGroup}/>
+				<label htmlFor=""> Todos</label>
+				<input type="checkbox"  value="fire" id="fire" onChange={handleSearchGroup}/>
 				<label htmlFor=""> Fuego</label>
-				<input type="radio" />
+				<input type="checkbox"  value="earth" id="earth" onChange={handleSearchGroup}/>
 				<label htmlFor=""> Tierra</label>
-				<input type="radio" />
+				<input type="checkbox"   value="air" id="air" onChange={handleSearchGroup}/>
 				<label htmlFor=""> Aire</label>
-				<input type="radio" />
-				<label htmlFor=""> Agua</label>
-			</form>
+				<input type="checkbox" value="agua" id="agua" />
+				<label htmlFor="" onClick={handleSearchGroup} value="agua"> Agua</label>
+			</form> */}
 			<div className="mainhoroscope">
 				{htmlData}
 			</div>
